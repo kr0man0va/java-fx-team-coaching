d5ee16fd48cacdbe8fa956d7639d32a8e05387bf
Qui Nguyen <quinguyen.dev@gmail.com>
2023-01-19 11:21:05 -0500

    [🆕] Initial commit for project files.

---
+++ src/main/java/edu/wpi/teamname/App.java
@@ -0,0 +1,24 @@
+package edu.wpi.teamname;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App extends Application {
+
+  @Override
+  public void init() {
+    log.info("Starting Up");
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.show();
+  }
+
+  @Override
+  public void stop() {
+    log.info("Shutting Down");
+  }
+}
